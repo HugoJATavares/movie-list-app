f3c9c77b0f256609e0783b1e3d7b96a269c99871
Hugo <hugotavares_13@hotmail.com>
2024-01-14 20:04:06 +0000

    Add nav bar and logo

---
+++ movie-list/src/App.js
@@ -0,0 +1,14 @@
+import {CssBaseline} from "@mui/material";
+
+import NavBar from "./components/NavBar"
+
+function App() {
+  return (
+    <div>
+      <CssBaseline/>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
